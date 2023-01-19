ef0ab84433a21594a3cfba06769398cb41579319
ErrorSaurabh <saurabhchauragade69@gmail.com>
2023-01-19 11:26:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavbarComp from './components/NavbarComp';
+import Card from './components/Card';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavbarComp/>
+      <Card title="Shri Narendra Modi" desc="Prime Minister Of India" button="Learn More" imgUrl="https://upload.wikimedia.org/wikipedia/commons/8/80/PM_at_First_Meeting_of_Central_Asia_Summit.jpg"/>
+      <Card title="Shri Amit Shah" desc="Ministry of Home Affairs" button="Learn More" imgUrl="https://businessdunia.in/wp-content/uploads/2022/03/amit-shah-at-scz-meet.jpg"/>
+      <Card title="Shri Rajnath Singh" desc="Ministry of Defence" button="Learn More" imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEavVBue5pu6zvslMZw3_ISbYZeUntVOzztqVNcQiyjTtAty5ID6P9NBuQkhq7onhJNdY&usqp=CAU"/>
+    </>
   );
 }
 
